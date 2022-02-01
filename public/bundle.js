f7a607ba08fbbe1037e496af769eebf6879dfd03
Luiz Ruiz <luizruiz04@gmail.com>
2022-02-01 19:19:07 -0400

    Operations with arrays

--- public/bundle.js
+++ public/bundle.js
@@ -1,3 +1,48 @@
 "use strict";
 
-console.log('Hello World');
+var numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function (number, index) {
+  numbers[index] = number + 1;
+});
+console.log(numbers);
+var newNumbers = numbers.map(function (number) {
+  return number + 1;
+});
+console.log(newNumbers);
+var total = numbers.reduce(function (total, next) {
+  return total + next;
+});
+console.log(total); // finish propertis with numbers
+
+var todos = [{
+  text: 'lavar a louça',
+  done: false
+}, {
+  text: 'apanhar lixo',
+  done: true
+}, {
+  text: 'dobrar roupa',
+  done: true
+}];
+var doneTodos = todos.filter(function (todo) {
+  return todo.done;
+});
+console.log(doneTodos);
+var donetodos = todos.filter(function (todo) {
+  return todo.text === 'dobrar roupa';
+});
+console.log(donetodos);
+var find = todos.find(function (todo) {
+  return todo.text === 'dobrar roupa';
+});
+console.log(find);
+var index = todos.findIndex(function (todo) {
+  return todo.text === 'apanhar lixo';
+});
+console.log(index); // finish propertis with todos
+
+var cars = ['fusca', 'civic', 'hb20'];
+var hasFusca = cars.some(function (car) {
+  return car === 'fusca';
+});
+console.log(hasFusca);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,63 @@
-console.log('Hello World')
+const numbers = [1,2,3,4,5]
+
+numbers.forEach(function (number, index) {
+    numbers[index] = number + 1
+})
+
+console.log(numbers)
+
+
+const newNumbers = numbers.map(function(number){
+    return number + 1
+
+})
+
+console.log(newNumbers)
+
+const total = numbers.reduce(function (total,next){
+    return total + next
+})
+
+console.log(total)
+
+// finish propertis with numbers
+
+const todos = [
+    { text : 'lavar a louça', done: false},
+    { text : 'apanhar lixo', done: true},
+    { text : 'dobrar roupa', done: true},
+]
+
+const doneTodos = todos.filter(function (todo){
+    return todo.done
+})
+
+console.log(doneTodos)
+
+const donetodos = todos.filter(function (todo){
+    return todo.text === 'dobrar roupa'
+})
+
+console.log(donetodos)
+
+const find = todos.find(function (todo) {
+    return todo.text === 'dobrar roupa'
+})
+
+console.log(find)
+
+const index = todos.findIndex(function(todo) {
+    return todo.text === 'apanhar lixo'
+})
+
+console.log(index)
+
+// finish propertis with todos
+
+const cars = ['fusca', 'civic', 'hb20']
+
+const hasFusca = cars.some(function (car){
+    return car === 'fusca'
+})
+
+console.log(hasFusca)
